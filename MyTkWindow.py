6b6ea047e4c961699c073e0bba3df820a2338378
Murtuza Quaizar <murtuzaq@gmail.com>
2021-09-11 20:20:42 -0700

    create class MyTkWindow. create window and left and right frame

---
+++ MyTkWindow.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+class MyTkWindow:
+    def __init__(self):
+        self.__draw_base_window()
+        self.__draw_left_frame()
+        self.__draw_right_frame()
+
+    def start(self):
+        self.root.mainloop()
+
+    def __draw_base_window(self):
+        self.root = tk.Tk()
+        self.root.title("Window Title")
+        self.root.config(background = "White")
+
+    def __draw_left_frame(self):
+        self.left_frame = tk.Frame(self.root, width = 200, height = 600)
+        self.left_frame.grid(row = 0, column = 0, padx = 10, pady = 2)
+
+    def __draw_right_frame(self):
+        self.right_frame = tk.Frame(self.root, width=200, height=300)
+        self.right_frame.grid(row=0, column=1, padx = 10, pady = 2)
---
+++ main.py
@@ -0,0 +1,10 @@
+import MyTkWindow
+
+def main():
+    print("Hello World")
+    my_window = MyTkWindow.MyTkWindow()
+    my_window.start()
+
+if __name__ == '__main__':
+    main()
+
